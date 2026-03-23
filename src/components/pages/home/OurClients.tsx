c685b57e5c97dc2f5714ff928d6941b1ec4cf0ea
fabriciodev26 <proyectos.ti@insalud.pe>
2026-03-23 15:59:41 -0500

    feat: add 'Our Clients' section with a client logo carousel and autoplay functionality.

--- src/components/pages/home/OurClients.tsx
+++ src/components/pages/home/OurClients.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
 import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
 
 export const OurClients = () => {
   return (
@@ -12,7 +15,17 @@ export const OurClients = () => {
         <h2 className="font-in-avantgarde text-2xl md:text-3xl lg:text-4xl text-center text-white pb-8 md:pb-16">
           Nuestros clientes
         </h2>
-        <Carousel className="w-full">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2500,
+            }),
+          ]}
+          opts={{
+            loop: true,
+          }}
+          className="w-full"
+        >
           <CarouselContent className="w-full max-w-7xl mx-auto -ml-4">
             <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
               <div
@@ -92,6 +105,104 @@ export const OurClients = () => {
                 <Image
                   src="/images/clients/lima-del-mar.png"
                   alt="Logo de Lima del Mar - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/18.png"
+                  alt="Logo de cliente 18 - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/19.png"
+                  alt="Logo de cliente 19 - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/20.png"
+                  alt="Logo de cliente 20 - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/21.png"
+                  alt="Logo de cliente 21 - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/22.png"
+                  alt="Logo de cliente 22 - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/23.png"
+                  alt="Logo de cliente 23 - Cliente de INB2B"
+                  width={100}
+                  height={100}
+                  className="w-auto h-24 mx-auto"
+                  unoptimized
+                />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="sm:basis-1/2 lg:basis-1/4 xl:basis-1/6">
+              <div
+                className="p-6 rounded-3xl h-[240px] flex flex-col justify-center"
+              >
+                <Image
+                  src="/images/clients/24.png"
+                  alt="Logo de cliente 24 - Cliente de INB2B"
                   width={100}
                   height={100}
                   className="w-auto h-24 mx-auto"
